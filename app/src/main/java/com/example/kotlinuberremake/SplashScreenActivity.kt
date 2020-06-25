d839efad4b1eabc2908cb2a7565f29c957cc1766
Iddan Gunawan <iddan.gunawan@tokodistributor.com>
2020-06-26 03:30:36 +0700

    - Authenticate User - Customize FirebaseUI - Sign Out session

--- app/src/main/java/com/example/kotlinuberremake/SplashScreenActivity.kt
+++ app/src/main/java/com/example/kotlinuberremake/SplashScreenActivity.kt
@@ -1,22 +1,104 @@
 package com.example.kotlinuberremake
 
 import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.Toast
+import com.firebase.ui.auth.AuthMethodPickerLayout
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import io.reactivex.Completable
 import io.reactivex.android.schedulers.AndroidSchedulers
+import kotlinx.android.synthetic.main.layout_sign_in.*
+import java.util.*
 import java.util.concurrent.TimeUnit
 
 class SplashScreenActivity : AppCompatActivity() {
 
-    @SuppressLint("CheckResult")
+    companion object {
+        val LOGIN_REQUEST_CODE = 7171
+    }
+
+    private lateinit var providers: List<AuthUI.IdpConfig>
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var listener: FirebaseAuth.AuthStateListener
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        init()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        delaySplashScreen()
+    }
+
+    override fun onStop() {
+        if (firebaseAuth != null && listener != null) firebaseAuth.removeAuthStateListener(listener)
+        super.onStop()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == LOGIN_REQUEST_CODE) {
+            val response = IdpResponse.fromResultIntent(data)
+            if (resultCode == Activity.RESULT_OK) {
+                val user = FirebaseAuth.getInstance().currentUser
+                Log.d("wkwkw", "${user?.displayName} | ${user?.phoneNumber} | ${user?.email}")
+            } else {
+                Toast.makeText(this, "Sorry, ${response?.error?.message}", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    private fun init() {
+        providers = arrayListOf(
+            AuthUI.IdpConfig.PhoneBuilder().build(),
+            AuthUI.IdpConfig.GoogleBuilder().build()
+        )
+        firebaseAuth = FirebaseAuth.getInstance()
+        listener = FirebaseAuth.AuthStateListener { myFireBaseAuth ->
+            val user = myFireBaseAuth.currentUser
+            if (user != null) {
+                Toast.makeText(this, "Welcome, ${user.uid}", Toast.LENGTH_SHORT).show()
+                startActivity((Intent(this, HomeActivity::class.java)))
+                finish()
+            } else {
+                showLoginLayout()
+            }
+        }
+    }
+
+    @SuppressLint("CheckResult")
+    private fun delaySplashScreen() {
         Completable.timer(3, TimeUnit.SECONDS, AndroidSchedulers.mainThread())
             .subscribe {
-                Toast.makeText(this, "Splash screen", Toast.LENGTH_SHORT).show()
+                firebaseAuth.addAuthStateListener(listener)
             }
+    }
+
+    private fun showLoginLayout() {
+        val authMethodPickerLayout = AuthMethodPickerLayout.Builder(R.layout.layout_sign_in)
+            .setPhoneButtonId(R.id.btn_phone_sign_in)
+            .setGoogleButtonId(R.id.btn_google_sign_in)
+            .build()
+
+        startActivityForResult(
+            AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAuthMethodPickerLayout(authMethodPickerLayout)
+                .setTheme(R.style.LoginTheme)
+                .setAvailableProviders(providers)
+                .setIsSmartLockEnabled(false)
+                .build()
+            , LOGIN_REQUEST_CODE
+        )
     }
 }
